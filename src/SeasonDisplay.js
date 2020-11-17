a4741f784c3f40b187850c09fc2edd44954c9361
HughesSeanKyle <khughessean@yahoo.com>
2020-11-17 15:14:21 +0200

    V65 - Showing Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -8,13 +8,16 @@ const getSeason = (lat, month) => {
   }
 };
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? 'Brrrrr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
     <div>
-    <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   ); 
 };
